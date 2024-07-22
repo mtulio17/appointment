8565051568511d5aaa74d241e6f07e46577a9445
germansosagoy <germanesosagoy@gmail.com>
2024-07-22 18:34:59 -0300

    feat: implement JWT & config auth controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ app.use(cors());
 
 // Middleware
 app.use(morgan("dev"));
+app.use(express.json());
 
 // Conectar a MongoDB
 const connectDB = async () => {
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,37 @@
-export const register = (req, res) => res.send('register');
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
-export const login = (req, res) => res.send('login');
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  console.log(username, email, password);
+
+  try {
+    const passwordHashed = await bcrypt.hash(password, 14);
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHashed,
+    });
+    const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+
+    res.json({
+      id: userSaved.id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const login = (req, res) => res.send("login");
+
+// export const profile = (req, res) => {
+//     const { user } = req;
+//     res.send(user);
+// };
