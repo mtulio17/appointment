9527a1b4a4ba67fcd969aa25e373baeb8678c84a
germansosagoy <germanesosagoy@gmail.com>
2024-11-25 16:39:07 -0300

    refactor: setSupabaseSession

--- client/src/utils/supabase.js
+++ client/src/utils/supabase.js
@@ -6,13 +6,8 @@ const supabaseKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const setSupabaseSession = async (supabaseAccessToken) => {
-  if (supabaseAccessToken) {
-    // console.log("Configurando la sesión de Supabase con el token:", supabaseAccessToken);
     await supabase.auth.setSession({ access_token: supabaseAccessToken });
-    // console.log("Sesión de Supabase configurada con éxito.");
-  } else {
-    console.error("No se proporcionó un token de acceso válido.");
-  }
+    console.log("Sesión de Supabase configurada con éxito.");
 };
 
 
