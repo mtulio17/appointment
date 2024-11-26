fe5e8673710374b675f9576a1e3f1703a160375e
germansosagoy <germanesosagoy@gmail.com>
2024-11-26 17:49:47 -0300

    feat(modal-context) add types email confirm & manage particiapants

--- client/src/context/ModalContext.jsx
+++ client/src/context/ModalContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState, useContext, useEffect } from 'react';
-import EventModal from '../components/modal/EventModal';
-import { EmailConfirmationModal } from '../components/modal/EmailConfirmationModal ';
+import { createContext, useState, useContext, useEffect } from "react";
+import EventModal from "../components/modal/EventModal";
+import ManageParticipantsModal from "../components/modal/ManageParticipantsModal";
+import { EmailConfirmationModal } from "../components/modal/EmailConfirmationModal ";
 
 const ModalContext = createContext();
 
@@ -8,36 +9,47 @@ export const ModalProvider = ({ children }) => {
   const [modalData, setModalData] = useState(null);
 
   const openModal = (event) => {
-    console.log('Abriendo modal con evento:', event);
+    // console.log("Abriendo modal con evento:", event);
     setModalData(event);
-    document.body.classList.add('overflow-hidden');
+    document.body.classList.add("overflow-hidden");
   };
-  
+
   const closeModal = () => {
     setModalData(null);
-    document.body.classList.remove('overflow-hidden');
+    document.body.classList.remove("overflow-hidden");
   };
 
   useEffect(() => {
     return () => {
-      document.body.classList.remove('overflow-hidden');
+      document.body.classList.remove("overflow-hidden");
     };
   }, []);
 
   return (
     <ModalContext.Provider value={{ modalData, openModal, closeModal }}>
       {children}
-      
-      {modalData?.type === 'edit' && (
+
+      {modalData?.type === "edit" && (
         <EventModal event={modalData.event} onClose={closeModal} />
       )}
 
-      {modalData?.type === 'emailConfirmation' && (
-        <EmailConfirmationModal event={modalData.event} onClose={closeModal} onConfirm={modalData.onConfirm} />
+      {modalData?.type === "manageParticipants" && (
+        <ManageParticipantsModal
+          participants={modalData.event?.participants || []}
+          event={modalData.event}
+          onClose={closeModal}
+        />
+      )}
+
+      {modalData?.type === "emailConfirmation" && (
+        <EmailConfirmationModal
+          event={modalData.event}
+          onClose={closeModal}
+          onConfirm={modalData.onConfirm}
+        />
       )}
     </ModalContext.Provider>
   );
 };
 
 export const useModal = () => useContext(ModalContext);
-
