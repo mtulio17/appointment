b588de8d6fc7c5931446ca7ad0ab8826050606bc
germansosagoy <germanesosagoy@gmail.com>
2024-12-02 12:49:04 -0300

    refactor: update layout and styles typography

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div className="about-us p-8 max-w-4xl mx-auto rounded-lg my-28">
-      <h1 className="text-3xl font-bold mb-6 text-indigo-700">Sobre Nosotros</h1>
+    <div className="about-us p-8 max-w-6xl mx-auto rounded-lg my-28 text-justify">
+      <h1 className="text-3xl font-bold mb-6 text-ambar-700">Sobre Nosotros</h1>
 
-      <h2 className="text-2xl font-semibold mb-4 text-indigo-600">La historia de Appointment</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-ambar-600">La historia de Appointment</h2>
       <p className="mb-6 text-gray-700">
         Appointment nació en el año 2022, cuando un grupo de amigos se dio cuenta de lo difícil que era encontrar actividades y eventos locales para conocer gente nueva y disfrutar de experiencias significativas. La idea surgió durante una conversación informal en un café, donde los fundadores discutían la necesidad de una plataforma que no solo facilitara la búsqueda de eventos, sino que también creara un espacio donde las personas pudieran conectar y compartir intereses comunes.
       </p>
 
-      <h2 className="text-2xl font-semibold mb-4 text-indigo-600">Misión</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-ambar-600">Misión</h2>
       <p className="mb-6 text-gray-700">
         Nuestra misión es sencilla: conectar a las personas con experiencias que enriquezcan sus vidas. Creemos que las actividades sociales, culturales y recreativas son esenciales para el bienestar y el crecimiento personal, y por eso creamos Appointment, una plataforma donde puedes descubrir eventos locales, unirte a actividades que te interesan y conocer a personas con las mismas pasiones.
       </p>
 
-      <h2 className="text-2xl font-semibold mb-4 text-indigo-600">Creciendo juntos</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-ambar-600">Creciendo juntos</h2>
       <p className="mb-6 text-gray-700">
         Desde nuestro lanzamiento, hemos crecido de ser una pequeña idea en una conversación de café a una comunidad global con miles de usuarios activos. A lo largo del tiempo, hemos ampliado nuestras funciones, ofreciendo más formas de descubrir eventos, crear tus propias actividades y unirte a grupos locales. Appointment ha pasado de ser una simple herramienta para organizar eventos a una plataforma integral para crear conexiones significativas.
       </p>
 
-      <h2 className="text-2xl font-semibold mb-4 text-indigo-600">Nuestra evolución</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-ambar-600">Nuestra evolución</h2>
       <p className="mb-6 text-gray-700">
         En nuestros primeros días, nos centramos exclusivamente en eventos sociales pequeños, pero pronto nos dimos cuenta de que los usuarios querían más. Ampliamos nuestra oferta para incluir actividades deportivas, culturales, recreativas y eventos empresariales, convirtiéndonos en el punto de encuentro para todo tipo de eventos.
       </p>
@@ -28,7 +28,7 @@ const AboutUs = () => {
         A medida que crecemos, también estamos comprometidos con la seguridad y la privacidad de nuestra comunidad. Trabajamos arduamente para garantizar que cada usuario se sienta cómodo y respaldado en nuestra plataforma. Hemos implementado medidas de seguridad avanzadas y escuchamos continuamente los comentarios de nuestros usuarios para mejorar nuestra oferta.
       </p>
 
-      <h2 className="text-2xl font-semibold mb-4 text-indigo-600">El futuro de Appointment</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-ambar-600">El futuro de Appointment</h2>
       <p className="mb-6 text-gray-700">
         Nuestro objetivo es seguir innovando. Creemos en el poder de las conexiones humanas y estamos comprometidos a hacer que sea más fácil que nunca encontrar experiencias únicas y enriquecedoras. Estamos explorando nuevas formas de facilitar las interacciones entre los usuarios, integrando nuevas tecnologías para personalizar aún más la experiencia de cada persona. 
       </p>
@@ -36,10 +36,13 @@ const AboutUs = () => {
         En los próximos años, esperamos expandir nuestra plataforma a más ciudades y países, con el sueño de convertirnos en la principal plataforma global para conectar personas a través de eventos y actividades.
       </p>
 
-      <h2 className="text-2xl font-semibold mb-4 text-indigo-600">Únete a nosotros</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-ambar-600">Únete a nosotros</h2>
       <p className="mb-6 text-gray-700">
         Appointment no sería nada sin nuestra comunidad. Cada evento organizado, cada conexión creada y cada nueva experiencia compartida es el corazón de lo que somos. Te invitamos a unirte a nosotros y a ser parte de esta emocionante aventura, donde el próximo evento podría cambiar tu vida.
       </p>
+      <footer className="mt-20">
+        <p className="lg:text-lg text-gray-600 font-semibold uppercase">Gracias por visitar Appointment 👋!</p>
+      </footer>
     </div>
   );
 };
