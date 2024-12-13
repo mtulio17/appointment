0d3b48814dd7136c8c2af8456c69188499715d51
germansosagoy <germanesosagoy@gmail.com>
2024-12-13 11:55:07 -0300

    feat(home): add new component join-appointment

--- client/src/layout/Home.jsx
+++ client/src/layout/Home.jsx
@@ -4,13 +4,14 @@ import Footer from "../components/Footer";
 // Carga diferida del componente
 const HeroSection = lazy(() => import(/* webpackPrefetch: true */ "../components/HeroSection"));
 const LastestEvents = lazy(() => import(/* webpackPrefetch: true */ "../components/LastestEvents"));
+const JoinAppointment = lazy(() => import(/* webpackPrefetch: true */ "../components/JoinAppointment"));
 const PopularCategories = lazy(() => import("../components/PopularCategories"));
 const HowItWorks = lazy(() => import("../components/HowItWorks"));
 // const EventModal = React.lazy(() => import("../components/EventModal"));
 
 const LoadingSkeleton = () => (
   <div className="loading-container">
-    <BarLoader color="#2C2C2C" width={"100%"} />
+    <BarLoader color="#2C2C2C" width={"80%"} />
   </div>
 );
 const Home = () => {
@@ -23,12 +24,17 @@ const Home = () => {
         <LastestEvents />
       </Suspense>
       <Suspense fallback={<LoadingSkeleton />}>
+        <JoinAppointment />
+      </Suspense>
+      <Suspense fallback={<LoadingSkeleton />}>
         <PopularCategories />
       </Suspense>
       <Suspense fallback={<LoadingSkeleton />}>
         <HowItWorks />
       </Suspense>
-      <Footer />
+      <Suspense fallback={<LoadingSkeleton />}>
+        <Footer />
+      </Suspense>
     </>
   );
 };
